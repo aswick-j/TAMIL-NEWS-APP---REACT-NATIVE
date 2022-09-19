5e2a63857072f6bc0afbc46a5a740c6a40c8ebb6
aswick <aswickjothi27@gmail.com>
2022-09-19 17:48:20 +0530

    sss

---
+++ src/components/WeatherCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyleSheet, Text, View,Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Weather = () => {
+  return (
+    <SafeAreaView>
+    <Image     style={{
+                    resizeMode: 'center',
+                    width: '100%',
+                    height: '100%',
+                   
+                  }} source={require('../assets/NicePng_coming-soon-png_46738.png')}></Image>
+    </SafeAreaView>
+  )
+}
+
+export default Weather
