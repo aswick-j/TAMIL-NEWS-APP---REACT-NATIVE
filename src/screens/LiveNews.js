40df18cf34b6fa43f965eb79748cd2a105948839
aswick <aswickjothi27@gmail.com>
2022-09-30 17:50:51 +0530

    fgfg

--- src/screens/LiveNews.js
+++ src/screens/LiveNews.js
@@ -11,12 +11,14 @@ const LiveNews = () => {
       <FMlist />
       <Text style={styles.text2}>Youtube Videos</Text>
       {/* <YoutubePlaylist/> */}
-      {/* <Image     style={{
+      <Image     style={{
                     resizeMode: 'center',
-                    width: '100%',
-                    height: '100%',
+                    marginLeft:50,
+                    width: 300,
+                    height: 300,
+                    marginTop:100
                    
-                  }} source={require('../assets/NicePng_coming-soon-png_46738.png')}></Image> */}
+                  }} source={require('../assets/NicePng_coming-soon-png_46738.png')}></Image>
     </SafeAreaView>
   );
 };
